83aa562df9dcfbea447cec01de1b7041563fb1a3
Mario Ruiz <11815099+mariodruiz@users.noreply.github.com>
2026-04-28 12:16:00 +0100

    Update setup_riallto_linux.sh to Ubuntu 24.04.4

--- scripts/linux/setup_riallto_linux.sh
+++ scripts/linux/setup_riallto_linux.sh
@@ -39,10 +39,10 @@ for URL in "${URLS[@]}"; do
         fi
 done
 
-# Check that we are on Ubuntu24.04.3
+# Check that we are on Ubuntu24.04.4
 distro_info=$(lsb_release -d)
-if [[ $distro_info != *"Ubuntu 24.04.3"* ]]; then
-	echo "Riallto is only currently supported on Ubuntu 24.04.3"
+if [[ $distro_info != *"Ubuntu 24.04.4"* ]]; then
+	echo "Riallto is only currently supported on Ubuntu 24.04.4"
 	exit 1
 fi
 
